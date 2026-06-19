f4306b6036c7e08fbe5ec4970ec206ac921efc65
Irfandtya <37211199+irfanaditya@users.noreply.github.com>
2026-06-19 17:47:44 +0700

    Delete src directory

--- src/logo.ts
+++
@@ -1,9 +0,0 @@
-import * as ecs from '@8thwall/ecs'
-
-// This component is attached to the logo, which allows instances to be queried for by the reset 
-// button
-const Logo = ecs.registerComponent({name: 'logo'})
-
-export {
-  Logo, 
-}
